cb2baca8cbe33e82195939fd0bdfff32d9507043
AhmedKanoun90 <86410406+AhmedKanoun90@users.noreply.github.com>
2022-07-26 18:45:00 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def hello():
     hello = '<div style = "position:fixed; text-align:center; top:10px; width: 100%; height: 10px; font-size: 30px;">' \
-            '<p>Hello World!!</p></div>'
+            '<p>Hello World!</p></div>'
     return hello
 
 
